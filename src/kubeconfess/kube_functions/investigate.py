20c5d22cc18be2562107d66fc05316f9ab7fe46a
arnavtripathy <arnavtripathy98@gmail.com>
2026-09-13 13:22:12 +0100

    Pushed fix for investigate namespace

--- src/kubeconfess/kube_functions/investigate.py
+++ src/kubeconfess/kube_functions/investigate.py
@@ -39,6 +39,10 @@ def _parse_target(target: str) -> tuple:
         name = name.strip()
         namespace = namespace.strip()
 
+    #Fix for namespaces being specified as the target, e.g. "namespace/payments"
+    if kind == "namespace":
+        namespace = name
+
     return kind, name.strip(), namespace
 
 
